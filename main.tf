9e05896c4d34901f13f4587cc7617cd114e0f26b
adam-ics <adam.ishak@icscompute.com>
2023-11-18 16:42:18 +0700

    testscpv1

--- main.tf
+++ main.tf
@@ -8,9 +8,30 @@ module "scp_root" {
   ou_list   = [data.aws_organizations_organizational_units.ou.id]
 }
 
+module "scp_core" {
+  source    = "./modules/scp"
+  for_each  = fileset(path.root, "policies/scp_core/*.json")
+  json_file = each.value
+  ou_list   = var.workload_ou
+}
+
+module "scp_infrastructure" {
+  source    = "./modules/scp"
+  for_each  = fileset(path.root, "policies/scp_infrastructure/*.json")
+  json_file = each.value
+  ou_list   = var.workload_ou
+}
+
 module "scp_workload" {
   source    = "./modules/scp"
   for_each  = fileset(path.root, "policies/scp_workload/*.json")
+  json_file = each.value
+  ou_list   = var.workload_ou
+}
+
+module "scp_sandbox" {
+  source    = "./modules/scp"
+  for_each  = fileset(path.root, "policies/scp_sandbox/*.json")
   json_file = each.value
   ou_list   = var.workload_ou
 }
